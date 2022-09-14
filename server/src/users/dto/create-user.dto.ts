54f860a25c6f3e78f4cb634a87c7a00dc83283d7
Bohdan <bohdan@MacBook-Pro-Bohdan.local>
2022-09-15 00:18:21 +0300

    validation

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,10 +1,9 @@
-import { IsEmail, IsString, Length } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString({ message: 'Should be a string' })
   @IsEmail({}, { message: 'Uncorrect email' })
   readonly email: string;
   @IsString({ message: 'Should be a string' })
-  @Length(4, 16, { message: 'Not less than 4 and not more than 16' })
   readonly password: string;
 }
